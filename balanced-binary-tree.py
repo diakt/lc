158ceb428827d79aa0d36efb239811ca29fed641
diakt <igs2107@columbia.edu>
2022-07-14 22:24:04 -0400

    t2

--- balanced-binary-tree.py
+++ balanced-binary-tree.py
@@ -32,6 +32,34 @@ class Solution(object):
         
         return dingo[0]
 
+#later shot, some variability but quick learning pattern
+#feel could maybe play around with rem left, right def for mem but unsure how to call once and check conditions based on both arr 0, 1 at same time w/o calling twice
+# 60 rt, 36 mem
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """        
+        def dfs(node):
+            if node is None:
+                return [True, 0]
+            else:
+                left = dfs(node.left)
+                right = dfs(node.right)
+                balanced = (left[0] and right[0] and abs(left[1]-right[1])<2)
+                return [balanced, max(left[1],right[1])+1]
+            
+        return dfs(root)[0]
+       
         
+                
         
         
